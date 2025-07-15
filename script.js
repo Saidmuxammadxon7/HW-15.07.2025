6ccc8a6ec554ca94bc0626eefc062252954d776e
Saidmuxammadxon7 <nothingusethisis@gmail.com>
2025-07-15 22:44:10 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log(27373)
+console.log("Ismail Jonkahone")
+
+// alert('Привет Саидухаммадхон')
+// alert('Пока')
+
+var name = "Alex"; // старый способ
+let age = 25; // современный, изменяемый
+const PI = 3.14; // константа, нельзя менять
+
+// camelCase - стандарт
+
+let userName = "Alex";
+let productPrice = 5000;
+
+// новая строка в console.log()
+console.log("Первая строка \n\nВторая строка");
+
+let user = prompt("Как вас зовут?");
+alert("Имя," + user);   
+
+let surname = prompt("Какая у вас фамиля?");
+alert("Фамиля," + surname); 
+
+let old = prompt("Сколько вам лет?");
+alert("Возраст," + old);
+
+let place = prompt("Где вы живёте?");
+alert("Место," + place);
+
+let learn = prompt("Где вы учитесь?");
+alert("Учеба," + learn);
